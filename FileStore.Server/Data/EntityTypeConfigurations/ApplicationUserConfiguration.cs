9e804a9ab5fa3e0be1b6c6b12125f8c88d363439
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-12 13:14:34 +0300

    feat(data): create entity type configuration for ApplicationUser

---
+++ FileStore.Server/Data/EntityTypeConfigurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using FileStore.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FileStore.Server.Data.EntityTypeConfigurations
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasKey(user => user.Id);
+
+            builder.ToTable("application_users");
+
+            builder.HasIndex(user => user.Name)
+                .IsUnique();
+
+            builder.Property(user => user.Name)
+                .HasMaxLength(256)
+                .IsRequired();
+
+            builder.Property(user => user.Email)
+                .HasMaxLength(256)
+                .IsRequired();
+        }
+    }
+}
