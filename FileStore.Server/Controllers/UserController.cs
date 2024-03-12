1fe0f560bef8a9d0ec96996f1e89bb14ca01a795
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-12 13:30:32 +0300

    feat: create empty user controller

---
+++ FileStore.Server/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileStore.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult SignUp()
+        {
+            return Ok();
+        }
+
+        public IActionResult SignIn()
+        {
+            return Ok();
+        }
+
+        public IActionResult LogOut()
+        {
+            return Ok();
+        }
+    }
+}
