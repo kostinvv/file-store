d62bf788057215475d24882da42f39dee6e80f8d
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-12 12:33:58 +0300

    feat(model): create `ApplicationUser.cs`

---
+++ FileStore.Server/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿namespace FileStore.Server.Models
+{
+    public class ApplicationUser
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PasswordHash { get; set; }
+    }
+}
