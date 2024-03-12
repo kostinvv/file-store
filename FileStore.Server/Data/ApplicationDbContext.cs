b7a9db800f56fe3bc0820d94fe3bbc17f633d8f5
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-12 13:13:47 +0300

    feat(data): add ef core

---
+++ FileStore.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FileStore.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FileStore.Server.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<ApplicationUser> Users => Set<ApplicationUser>();
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- FileStore.Server/Program.cs
+++ FileStore.Server/Program.cs
@@ -1,6 +1,14 @@
+using FileStore.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(connectionString: 
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
