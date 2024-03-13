1057e222e2af869d3307102f6038d450ced431e7
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-13 20:37:21 +0300

    feat(service): create UserService & impl. method CreateUserAsync

---
+++ FileStore.Server/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using FileStore.Server.DTOs.User;
+using FileStore.Server.Results;
+
+namespace FileStore.Server.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<Result> CreateUserAsync(CreateUserRequest request);
+
+        public Task<UserLoginResponse> LoginUserAsync(UserLoginRequest request);
+    }
+}
---
+++ FileStore.Server/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using FileStore.Server.Data;
+using FileStore.Server.DTOs.User;
+using FileStore.Server.Errors;
+using FileStore.Server.Models;
+using FileStore.Server.Results;
+using FileStore.Server.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileStore.Server.Services
+{
+    public class UserService(ApplicationDbContext context) : IUserService
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<Result> CreateUserAsync(CreateUserRequest request)
+        {
+            var foundUser = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(user => user.Name == request.Name);
+
+            if (foundUser is not null)
+            {
+                return Result.Failure(UserErrors.UserAlreadyExists);
+            }
+
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            await _context.Users.AddAsync(new ApplicationUser { 
+                Name = request.Name,
+                Email = request.Email, 
+                PasswordHash = passwordHash, 
+            });
+            await _context.SaveChangesAsync();
+
+            return Result.Success();
+        }
+
+        public Task<UserLoginResponse> LoginUserAsync(UserLoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
