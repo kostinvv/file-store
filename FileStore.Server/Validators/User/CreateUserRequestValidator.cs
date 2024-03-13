20a3e5e6f83c7eb4d6394fe3bc9922e3050ac3d2
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-13 20:35:14 +0300

    feat: add `CreateUserRequestValidator.cs`

---
+++ FileStore.Server/Validators/User/CreateUserRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FileStore.Server.DTOs.User;
+using FluentValidation;
+
+namespace FileStore.Server.Validators.User
+{
+    public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+    {
+        public CreateUserRequestValidator()
+        {
+            RuleFor(user => user.Name)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(user => user.Email)
+                .NotEmpty()
+                .NotNull()
+                .EmailAddress();
+
+            RuleFor(user => user.Password)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(user => user.Password)
+                .Equal(user => user.PasswordConfirm)
+                .WithMessage("Passwords must match");
+        }
+    }
+}
