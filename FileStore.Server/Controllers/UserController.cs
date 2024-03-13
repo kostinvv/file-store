8eb73f612fc97885fdbe699b842d790321a9fa7a
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-13 20:40:13 +0300

    feat(UserController.cs): update a signup method

--- FileStore.Server/Controllers/UserController.cs
+++ FileStore.Server/Controllers/UserController.cs
@@ -1,21 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FileStore.Server.DTOs.User;
+using FileStore.Server.Exceptions.User;
+using FileStore.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FileStore.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/user")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UserController(IUserService userService) : ControllerBase
     {
-        public IActionResult SignUp()
+        [HttpPost("sign-up")]
+        public async Task<ActionResult> SignUp(
+            [FromBody] CreateUserRequest request)
         {
-            return Ok();
+            var result = await userService.CreateUserAsync(request);
+
+            if (result.IsSuccess)
+            {
+                return Created();
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Message);
+                }
+            }
+
+            return ValidationProblem();
         }
 
+        [HttpPost("sign-in")]
         public IActionResult SignIn()
         {
             return Ok();
         }
 
+        [HttpPost("logout")]
         public IActionResult LogOut()
         {
             return Ok();
