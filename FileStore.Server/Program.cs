b5ca93434577167d492c601efb23c7fbbe178502
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-13 20:38:52 +0300

    feat(Program.cs): add service to container

--- FileStore.Server/Program.cs
+++ FileStore.Server/Program.cs
@@ -1,14 +1,31 @@
 using FileStore.Server.Data;
+using FileStore.Server.DTOs.User;
+using FileStore.Server.Services;
+using FileStore.Server.Services.Interfaces;
+using FileStore.Server.Validators.User;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddScoped<IValidator<CreateUserRequest>, CreateUserRequestValidator>();
+
+builder.Services.AddFluentValidationAutoValidation();
+
+ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("en-US");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(connectionString: 
         builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//builder.Services.AddProblemDetails();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +43,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
