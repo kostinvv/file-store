582658b7e2c0fe61f5e4f12c7a79d296ecf08d15
Vladislav <vladislavkostin.ds@yandex.ru>
2024-03-13 20:33:35 +0300

    feat: create `Result.cs` for service responses

---
+++ FileStore.Server/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using FileStore.Server.Errors;
+
+namespace FileStore.Server.Results
+{
+    public class Result
+    {
+        private Result(List<Error> errors)
+        {
+            Errors = errors ?? [];
+        }
+
+        public bool IsSuccess => Errors.Count == 0;
+
+        public IReadOnlyList<Error> Errors { get; }
+
+        public static Result Success() => new([]);
+
+        public static Result Failure(Error error) => new([error]);
+
+        public static Result Failure(IEnumerable<Error> errors) => 
+            new(new List<Error>(errors));
+    }
+}
