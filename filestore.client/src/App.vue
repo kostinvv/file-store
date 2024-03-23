f914fab5a052e64c976e233660e863572e675f1f
Vladislav Kostin <148951284+kostinvv@users.noreply.github.com>
2024-03-23 20:12:24 +0300

    update app.vue

--- filestore.client/src/App.vue
+++ filestore.client/src/App.vue
@@ -1,12 +1,36 @@
 <script setup>
+  import { useAuthStore } from "@/stores/auth.js";
+
   import TheHeader from "@/components/TheHeader.vue";
+  
+  const authStore = useAuthStore();
+  
+  const checkUser = () => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    
+    if (userInfo) {
+      authStore.userInfo.accessToken = userInfo.token;
+      authStore.userInfo.username = userInfo.username;
+    }
+  }
+  
+  checkUser();
 </script>
 
 <template>
-  <div class="w-3/5 m-auto mt-14">
-    <the-header/>
-    <div class="mt-10">
-      <router-view/>
+  <div class="flex flex-col h-screen">
+    <div class="container m-auto mt-14">
+      <the-header/>
+      <!-- content -->
+      <main class="mt-20">
+        <router-view/>
+      </main>
     </div>
+    <!-- sticky footer -->
+    <footer class="p-8">
+      <b>Github: </b>
+      <a class="hover:text-blue-700"
+         href="https://github.com/kostinvv/file-store">file-store</a>
+    </footer>
   </div>
 </template>
