51b3628366c6536ce323559945579d026c357d06
Vladislav Kostin <148951284+kostinvv@users.noreply.github.com>
2024-03-24 15:43:22 +0300

    feat: create Add_StorageObjectModel migration

---
+++ FileStore.Server/Data/Migrations/20240324122519_Add_StorageObjectModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileStore.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_StorageObjectModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "storage_objects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Size = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FileType = table.Column<string>(type: "text", nullable: false),
+                    IsDirectory = table.Column<bool>(type: "boolean", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_storage_objects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_storage_objects_application_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "application_users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_storage_objects_Name",
+                table: "storage_objects",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_storage_objects_UserId",
+                table: "storage_objects",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "storage_objects");
+        }
+    }
+}
